178a6cc6628b885c3e41154100dedc98e355ecd4
Shivam Kureel <148866549+ShivamKureel1122@users.noreply.github.com>
2024-01-07 23:58:10 +0530

    Create day5-Verify-prime.cpp

---
+++ day5-Verify-prime.cpp
@@ -0,0 +1,8 @@
+// Problem - https://www.interviewbit.com/problems/verify-prime/
+--------------------------------------------------------
+int Solution::isPrime(int A) {
+    if(A == 1) return 0;
+    for(int i=2; i<A; i++) 
+        if(A%i == 0) return 0;
+    return 1;
+}
