4824b7668285a151508bbf57ad52d3ed7cbaeaf3
Shivam Kureel <148866549+ShivamKureel1122@users.noreply.github.com>
2024-01-12 11:04:01 +0530

    Update day5-Verify-prime.cpp

--- day5-Verify-prime.cpp
+++ day5-Verify-prime.cpp
@@ -2,7 +2,7 @@
 --------------------------------------------------------
 int Solution::isPrime(int A) {
     if(A == 1) return 0;
-    for(int i=2; i<A; i++) 
+    for(int i = 2; i <= A/2; i++) 
         if(A%i == 0) return 0;
     return 1;
 }
